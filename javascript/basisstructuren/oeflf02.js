01d615d6e855a6c2f2e0357d6ba11753486d6fe5
vanhemel <tom_vanhemel@yahoo.com>
2017-10-24 10:56:59 +0200

    oefeining 2 switch

--- javascript/basisstructuren/oeflf02.js
+++ javascript/basisstructuren/oeflf02.js
@@ -5,9 +5,26 @@ var operator
 var toetsenbord = require('readline-sync');
 
 var getal1 = toetsenbord.question('Geef een eerste getal:  ');
+var operator = toetsenbord.question('Geef een operator:  ');
+var getal2 = toetsenbord.question('Geef een tweede getal:  ');
 
-if (geld >5) {
-    console.log("Voor mij een ijsje met 3 bollen en slagroom aub");
-} else {
-    console.log("Ik zal wel een platte water drinken")
+
+getal1= parseInt(getal1)
+getal2= parseInt(getal2)
+
+switch (operator) {
+    case '+':
+        console.log("de som van de getallen is:",getal1+getal2);
+        break;
+    case '-':
+        console.log("Het verschil tussen de getallen is:",getal1-getal2);
+        break;
+    case '*':
+        console.log("Het product van de getallen is:",getal1*getal2);
+        break;
+    case '/':
+        console.log("Het quotient van de getallen is:",getal1/getal2);
+        break;  
+    default:
+        console.log("Deze bewerking ken ik niet.");
 }
