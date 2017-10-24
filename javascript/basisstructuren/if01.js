6b4cee3fd8f133225dedd24ffe7c34084effb9ae
vanhemel <tom_vanhemel@yahoo.com>
2017-10-24 10:01:43 +0200

    voorbeelden Logische operatoren

--- javascript/basisstructuren/if01.js
+++ javascript/basisstructuren/if01.js
@@ -7,3 +7,14 @@ if (uur < 12) {
 } else {
 	console.log("Goedenamiddag");
 }
+if (uur != 12) {
+	console.log("De middagpauze is niet bezig");
+} else {
+	console.log("Het is middagpauze");
+}
+if(uur >=9 && uur <=12){
+	console.log("de werkvoormiddag is bezig");
+}
+if(uur < 9 || uur > 18) {
+	console.log("buiten de werkuren");
+}
