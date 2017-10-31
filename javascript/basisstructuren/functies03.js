5dc56d29967c997382f5da168695192f48e25fdf
annve <ann.vaneyken@betavzw.org>
2017-10-31 09:26:59 +0100

    functies03 uitgebreid met tekenVierkant()

--- javascript/basisstructuren/functies03.js
+++ javascript/basisstructuren/functies03.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // een functie zonder expliciete return
-function tekenHuisje(){
+function tekenHuisje() {
     console.log("   *");
     console.log("  * *");
     console.log(" *   *");
@@ -17,3 +17,17 @@ tekenHuisje();
 // klassikale oef.:
 // schrijf een functie om een vierkant met doorgegeven
 // zijde te tekenen
+
+function tekenVierkant(zijde) {
+    var resultaat;
+    for (var rij = 1; rij <= zijde; rij++) {
+        resultaat = "";
+        for (var i = 1; i <= zijde; i++) {
+            resultaat += "*";
+        }
+        console.log(resultaat);
+    }
+}
+
+tekenVierkant(4);
+tekenVierkant(10);
