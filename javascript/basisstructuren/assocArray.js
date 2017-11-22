b38e5ebe7da3d004076e5930a0aa59128576b737
annve <ann.vaneyken@betavzw.org>
2017-11-22 12:01:20 +0100

    assocArray.js aangepast

--- javascript/basisstructuren/assocArray.js
+++ javascript/basisstructuren/assocArray.js
@@ -1,4 +1,4 @@
-'use strict';
+    'use strict';
 
 var toetsenbord = require('readline-sync');
 
@@ -6,8 +6,15 @@ var lijst = new Array();
 var naam;
 for (var i = 0; i < 5; i++) {
     naam = toetsenbord.question("Geef een naam: ");
-    if (lijst[naam] == null) lijst[naam] = 0;
-    lijst[naam]++;
+    // if (lijst[naam] == null) lijst[naam] = 0;
+    // lijst[naam]++;
+    // of:
+    if (lijst[naam] == null) {
+        lijst[naam] = 1;
+    }
+    else{
+        lijst[naam]++;
+    }
 }
 for (naam in lijst) {  // overloop alle indexen
     console.log("%s komt %d keer voor.", naam, lijst[naam]);
