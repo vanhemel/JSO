41c36e64910ffddbf1a66005ca4b0cc163002ee1
annve <ann.vaneyken@betavzw.org>
2017-11-23 16:44:06 +0100

    obp05.js uitgebreid met extra's

--- javascript/OOP/obp05.js
+++ javascript/OOP/obp05.js
@@ -20,3 +20,16 @@ auto.aantalUren = 2;
 auto.aantalMinuten = 5;
 console.log("Na %d:%s u gereden te hebben tegen %d km/uur heb je %d km afgelegd",
     auto.aantalUren, auto.aantalMinuten < 10 ? "0"+auto.aantalMinuten : auto.aantalMinuten,  auto.snelheid, auto.afstand());
+ console.log( auto["aantalUren"]);
+
+ for(var smurf in auto){
+    console.log("propertie %s heeft waarde %s", smurf, auto[smurf]);
+ }
+
+ delete auto.snelheid;
+ console.log(auto);
+
+ console.log(auto.smurf);
+ if (auto.smurf != undefined){
+     // auto heeft propery smurf
+ }
