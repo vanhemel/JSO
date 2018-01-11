56be3773d1d517a99e753b4ed58603d7da0fee8d
annve <ann.vaneyken@betavzw.org>
2018-01-11 09:51:32 +0100

    event-loop.js: argument toegevoegd aan eventhandler van 'data_received'

--- Node.js/event-loop.js
+++ Node.js/event-loop.js
@@ -7,15 +7,16 @@ var connectHandler = function connected() {
    console.log('connection succesful.');
   
    // Fire the data_received event 
-   eventEmitter.emit('data_received');
+   eventEmitter.emit('data_received', "dag smurfen");
 };
 
 // Bind the connection event with the handler
 eventEmitter.on('connection', connectHandler);
  
 // Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', function(){
+eventEmitter.on('data_received', function(a){
    console.log('data received succesfully.');
+   console.log(a);
 });
 
 // Fire the connection event 
