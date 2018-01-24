676f0743413afe80abbf4e2b8c1208b62fb6d1ce
vanhemel <tom_vanhemel@yahoo.com>
2018-01-24 14:06:41 +0100

    Oef nodeJS resto

---
+++ Node.js/server-express-htmlTom.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var url = require("url");
+
+app.get('/*.htm*', function(req, res){
+    res.sendFile( path.join(__dirname + url.parse(req.url).pathname));
+});
+
+var server = app.listen(1338, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log("Example app listening at http://%s:%s", host, port)
+});
