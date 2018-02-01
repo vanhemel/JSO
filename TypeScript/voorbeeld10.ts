09b3631158482fcad01e3c82fef9ede7b163f164
annve <ann.vaneyken@betavzw.org>
2018-02-01 17:10:35 +0100

    voorbeeld10.ts uitgebreid

--- TypeScript/voorbeeld10.ts
+++ TypeScript/voorbeeld10.ts
@@ -11,9 +11,35 @@ class Persoon{
 /* INHERITANCE (OVERERVING) */
 class Student extends Persoon{
     opleiding: string;
+    protected studentnr: string = "NN";  // protected => toegankelijk in deze klasse en
+    // in de subklassen
+}
+
+class ErasmusStudent extends Student{
+    wijzigStudentNr(nieuwNr: string): void{
+        this.studentnr = nieuwNr;  
+    }
+}
+
+class Docent extends Persoon{
+    private loon: number = 5000;
+    verhoogLoon(bedrag: number){
+        this.loon += bedrag;
+    }
 }
 
 let student = new Student("Jos", "Vermeulen");
 student.voornaam += "ke";   // Student erft voornaam van Persoon
 student.opleiding = "informatica";
 console.log(student);
+// student.studentnr = "123";  // compileerfout; want studentnr is protected
+
+let chloe: ErasmusStudent = new ErasmusStudent("Chloe", "Claes");
+chloe.opleiding = "FEO";
+chloe.voornaam = "Chloé";
+console.log(chloe);
+
+let docent: Docent = new Docent("Prof", "Gobelijn");
+docent.verhoogLoon(100000);
+// docent.opleiding = "blabla";  // compileerfout; want Docent heeft geen veld opleiding
+console.log(docent);
