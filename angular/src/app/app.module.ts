a1d45656c088139a704804d40114d1ba6fd2177c
annve <ann.vaneyken@betavzw.org>
2018-02-08 09:46:56 +0100

    oef00 toegevoegd

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  // nodig voor ngModel (-> 2 way binding)
 
 import { AppComponent } from './app.component';
+import { Oef00Component } from './oefeningen/oef00/oef00.component';
 // TODO: alle componenten die in deze module gedefinieerd zijn importeren
 
 @NgModule({
   declarations: [
     AppComponent,
+    Oef00Component,
     // TODO: alle componenten die in deze module gedefinieerd zijn hier importeren
   ],
   imports: [
@@ -17,6 +19,6 @@ import { AppComponent } from './app.component';
   providers: [],
   // TODO: in bootstrap array (enkel) alle componenten opsommen
   // die horen bij user defined tags die in index.html gebruikt worden
-  bootstrap: [AppComponent]
+  bootstrap: [Oef00Component]
 })
 export class AppModule { }
