462bfe91619aece6c3adb2988eb29779b540d656
annve <ann.vaneyken@betavzw.org>
2018-02-12 11:26:14 +0100

    vbrouting toegevoegd

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -11,27 +11,62 @@ import { OefHogerLagerComponent } from './oefeningen/oefhogerlager/oefhogerlager
 import { OefCurrencyconvertorComponent } from './oefeningen/oefcurrencyconvertor/oefcurrencyconvertor.component';
 import { CurrencyService } from './oefeningen/oefcurrencyconvertor/currency.service';
 import { HttpClientModule } from '@angular/common/http';
-// TODO: alle componenten die in deze module gedefinieerd zijn importeren
+import { Routes, RouterModule } from '@angular/router';
+import { VbDatabindingComponent } from './voorbeelden/vbdatabinding/vbdatabinding.component';
+import { VbIfComponent } from './voorbeelden/vbif/vbif.component';
+import { VbroutingReqparamComponent } from './voorbeelden/vbrouting/vbrouting-reqparam.component';
+import { PageNotFoundComponent } from './voorbeelden/vbrouting/page-not-found.component';
+import { Vbrouting0Component } from './voorbeelden/vbrouting/vbrouting0.component';
+import { VbroutingComponent } from './voorbeelden/vbrouting/vbrouting.component';
+// TODO: alle componenten die in deze module gedefinieerd zijn importere
+  // zie vbrouting.module.ts
+const appRoutes: Routes = [
+  {
+    path: 'vb01',
+    component: VbDatabindingComponent
+  },
+  {
+    path: 'vb02',
+    component: VbIfComponent
+  },
+  {
+    path: 'routing/:id',
+    component: VbroutingReqparamComponent
+  },
+  { 
+    path: '',   // start path
+    redirectTo: '/vb01',  // REDIRECT
+    //  A redirect route requires a pathMatch property to tell the router 
+    //   how to match a URL to the path of a route. The router throws an error 
+    //   if you don't. 
+    pathMatch: 'full'  // whole URL must match
+  },
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  } 
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Oef00Component,
-    OefNotitieComponent,
-    VbForuitbrComponent,
-    OefBroodjesComponent,
-    OefHogerLagerComponent,
-    OefCurrencyconvertorComponent
+    VbDatabindingComponent,
+    VbIfComponent,
+    OefCurrencyconvertorComponent,
+    PageNotFoundComponent,
+    Vbrouting0Component,
+    VbroutingComponent,
+    VbroutingReqparamComponent
     // TODO: alle componenten die in deze module gedefinieerd zijn hier importeren
   ],
   imports: [
     BrowserModule,
     FormsModule,  //  nodig om 2 way binding te kunnen doen
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot( appRoutes ),
   ],
   providers: [CurrencyService],
   // TODO: in bootstrap array (enkel) alle componenten opsommen
   // die horen bij user defined tags die in index.html gebruikt worden
-  bootstrap: [OefCurrencyconvertorComponent]
+  bootstrap: [Vbrouting0Component]
 })
 export class AppModule { }
